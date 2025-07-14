604de9cec0e238cf3b61fa2c6f9f0d9aa86a0786
FatherOfSnails <fatherofsnails@gmail.com>
2025-07-13 21:33:58 -0400

    B: Added functions to stats and added new output to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_words, get_character_count, sort_stats
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -7,10 +8,22 @@ def get_book_text(file_path):
 
 
 def main():
-    word_count = get_words('books/frankenstein.txt')
-    character_count = get_character_count('books/frankenstein.txt')
+    path_to_file = "books/frankenstein.txt"
+    word_count = get_words(path_to_file)
+    character_count = get_character_count(path_to_file)
     stats = sort_stats(character_count)
-    print(f"{word_count} words found in the document")
-    print(stats)
+
+    print('============ BOOKBOT ============')
+    print("Analyzing book found at ", path_to_file, "...", sep='')
+
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words.")
+
+    print ("--------- Character Count -------")
+    for stat in stats:
+        if stat["char"].isalpha():
+            print(stat["char"] , ": " , stat["num"], sep='')
+    
+    print("============= END ===============")
 
 main()
