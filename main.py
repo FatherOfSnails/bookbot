f09a335d03e34bb69cbafd39b8ac3f20fce2443e
FatherOfSnails <fatherofsnails@gmail.com>
2025-07-13 22:28:57 -0400

    C: Added new books and sys arguments for filepaths

--- main.py
+++ main.py
@@ -8,22 +8,32 @@ def get_book_text(file_path):
 
 
 def main():
-    path_to_file = "books/frankenstein.txt"
-    word_count = get_words(path_to_file)
-    character_count = get_character_count(path_to_file)
-    stats = sort_stats(character_count)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
-    print('============ BOOKBOT ============')
-    print("Analyzing book found at ", path_to_file, "...", sep='')
+    elif len(sys.argv) == 2:
+        script_name = sys.argv[0]
+        path_to_file = sys.argv[1]
+        word_count = get_words(path_to_file)
+        character_count = get_character_count(path_to_file)
+        stats = sort_stats(character_count)
 
-    print("----------- Word Count ----------")
-    print(f"Found {word_count} total words.")
+        print('============ BOOKBOT ============')
+        print("Analyzing book found at ", path_to_file, "...", sep='')
 
-    print ("--------- Character Count -------")
-    for stat in stats:
-        if stat["char"].isalpha():
-            print(stat["char"] , ": " , stat["num"], sep='')
+        print("----------- Word Count ----------")
+        print(f"Found {word_count} total words.")
+
+        print ("--------- Character Count -------")
+        for stat in stats:
+            if stat["char"].isalpha():
+                print(stat["char"] , ": " , stat["num"], sep='')
     
-    print("============= END ===============")
+        print("============= END ===============")
+
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 main()
